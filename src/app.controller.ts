aa2cf7af36ab48fcfd9c19e0b6b4fbbc6547e410
Ceeh-Jay <chiemekaelumeziem@gmail.com>
2023-03-22 11:18:59 +0100

    implement basic CRUD endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,75 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ReportType } from './data';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
+import { data } from 'src/data';
+import {AppService} from './app.service';
 
-@Controller()
+@Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService
+  ){}
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllReport(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.getAllReports(reportType)
+  }
+
+  @Get(':id')
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.getReportById(reportType, id)
+  }
+
+  @Post()
+  createReport(
+    @Body()
+    {
+      source,
+      amount,
+    }: {
+      source: string;
+      amount: number;
+    },
+    @Param('type') type: string,
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, {amount, source})
+  }
+
+  @Put(':id')
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body()
+    body: {
+      source: string;
+      amount: number;
+    },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+      return this.appService.updateReport(reportType, id, body)
+    
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  deleteReport(
+    @Param('id') id : string
+  ) {
+    return this.appService.deleteReport(id);
   }
 }
